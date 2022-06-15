faa4a95ae742b3e2220d20e38f83974016bdad2e
rarora2025 <60102689+rarora2025@users.noreply.github.com>
2022-06-14 20:44:24 -0400

    Add files via upload

---
+++ Quizzler-iOS13-Completed/Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var MCButton2: UIButton!
+    @IBOutlet weak var MCButton3: UIButton!
+    @IBOutlet weak var MCButton1: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateUI()
+    }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        
+        let userGotItRight = quizBrain.checkAnswer(userAnswer: userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        
+        MCButton1.backgroundColor = UIColor.clear
+        MCButton2.backgroundColor = UIColor.clear
+        MCButton3.backgroundColor = UIColor.clear
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Quizzler-iOS13-Completed/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,24 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 07/08/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    let correct: String
+    
+    init(q: String, a: String, correctAnswer: String) {
+        text = q
+        answer = a
+        correct = correctAnswer
+
+    }
+}
+
+    
